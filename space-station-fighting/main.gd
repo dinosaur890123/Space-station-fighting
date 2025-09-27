70274a99dec4dfa276b1600fa27151f9eb574c49
dinosaur890123 <dinosaur890123@gmail.com>
2025-09-27 12:22:37 +1000

    add variables global

--- space-station-fighting/main.gd
+++ space-station-fighting/main.gd
@@ -1,1 +1,9 @@
-extends Node
+extends Control
+const MAX_CAPACITY = 100.0
+var current_battery: float = 100.0
+var max_battery: float = 100.0
+var shield_integrity: float = 50.0
+var signal_progress: float = 0.0
+var overcharge_count: int = 0
+var shield_boost_timer: float = 0.0
+var signal_boost_timer: float = 0.0
