2ec97e9133a9b3fb9bc995f0f57548307bc23774
Rknite <134701238+Rknite@users.noreply.github.com>
2025-09-27 11:58:58 +1000

    h

--- space-station-fighting/character.gd
+++ space-station-fighting/character.gd
@@ -1,4 +1,7 @@
 extends AnimatedSprite2D
+@export var speed: float = 300 
+@export var upper_limit: float = 62
+@export var lower_limit: float = 656
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,17 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var direction = 0
-	if Input.is_action_pressed("Bup"):
+	if Input.is_action_pressed("left"):
 		direction = -1  # Move up
-	elif Input.is_action_pressed("Bdown"):
+	elif Input.is_action_pressed("right"):
 		direction = 1  # Move down	
-	elif Input.is_action_pressed("Bswing"):
-		$Bpaddle.play()
-	elif Input.is_action_pressed("Bup") and Input.is_action_pressed("Bswing"):
-		$Bpaddle.play()
-		direction = -1 
-	elif Input.is_action_pressed("Bswing") and Input.is_action_pressed("Bdown"):
-		$Bpaddle.play()
-		direction = 1 
 	position.y += direction * speed * delta
 	position.y = clamp(position.y, upper_limit, lower_limit)
