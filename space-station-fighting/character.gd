20824b9339a2bd1d374f7661eda20860a5743de7
Rknite <134701238+Rknite@users.noreply.github.com>
2025-09-27 11:55:38 +1000

    h

--- space-station-fighting/character.gd
+++ space-station-fighting/character.gd
@@ -8,4 +8,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	var direction = 0
+	if Input.is_action_pressed("Bup"):
+		direction = -1  # Move up
+	elif Input.is_action_pressed("Bdown"):
+		direction = 1  # Move down	
+	elif Input.is_action_pressed("Bswing"):
+		$Bpaddle.play()
+	elif Input.is_action_pressed("Bup") and Input.is_action_pressed("Bswing"):
+		$Bpaddle.play()
+		direction = -1 
+	elif Input.is_action_pressed("Bswing") and Input.is_action_pressed("Bdown"):
+		$Bpaddle.play()
+		direction = 1 
+	position.y += direction * speed * delta
+	position.y = clamp(position.y, upper_limit, lower_limit)
