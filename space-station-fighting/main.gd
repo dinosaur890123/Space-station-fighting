3707127fe8ac365279a0a8d1965781c59793b9b0
dinosaur890123 <dinosaur890123@gmail.com>
2025-09-27 12:25:20 +1000

    a

--- space-station-fighting/main.gd
+++ space-station-fighting/main.gd
@@ -1,9 +1,3 @@
 extends Control
-const MAX_CAPACITY = 100.0
-var current_battery: float = 100.0
-var max_battery: float = 100.0
-var shield_integrity: float = 50.0
-var signal_progress: float = 0.0
-var overcharge_count: int = 0
-var shield_boost_timer: float = 0.0
-var signal_boost_timer: float = 0.0
+@onready var game_over_screen = $GameOverScreen
+@onready var result_message = $GameOverScreen/VBoxContainer/ResultMessage
