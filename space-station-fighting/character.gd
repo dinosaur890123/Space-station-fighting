a01b7c45d6a1f8cb2ddf32f662d104b9c198f55c
Rknite <134701238+Rknite@users.noreply.github.com>
2025-09-28 11:21:09 +1000

    a

--- space-station-fighting/character.gd
+++ space-station-fighting/character.gd
@@ -64,3 +64,11 @@ func _play_if_exists(anim_name: String) -> void:
 
 func force_face_right(right: bool):
 	_set_facing(right)
+
+
+func _on_area_area_entered(area: Area2D) -> void:
+	move_speed = 10 # Replace with function body.
+
+
+func _on_area_area_exited(area: Area2D) -> void:
+	move_speed = 300 # Replace with function body.
