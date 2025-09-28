f60bc9ea14a4794c92af7d4ca7b54236929888c5
duckonpc <ryan.truong100@gmail.com>
2025-09-28 15:27:50 +1000

    e

--- space-station-fighting/intro_screen.gd
+++ space-station-fighting/intro_screen.gd
@@ -4,13 +4,11 @@ extends Control
 @onready var tutorial_panel = $TutorialPanel
 @onready var tutorial_button = $TutorialButton
 @onready var close_button = $TutorialPanel/CloseButton
-@onready var quit_button = $QuitButton
 
 func _ready():
 	start_button.pressed.connect(_on_start_button_pressed)
 	tutorial_button.pressed.connect(_on_tutorial_button_pressed)
 	close_button.pressed.connect(_on_close_button_pressed)
-	quit_button.pressed.connect(_on_quit_button_pressed)
 	tutorial_panel.hide()
 
 func _on_start_button_pressed():
@@ -21,6 +19,3 @@ func _on_tutorial_button_pressed():
 
 func _on_close_button_pressed():
 	tutorial_panel.hide()
-
-func _on_quit_button_pressed():
-	get_tree().quit()
