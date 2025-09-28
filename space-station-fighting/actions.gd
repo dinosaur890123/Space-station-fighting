df5ccc51dc0f5bdbd91c66fa1919b99e44ce3636
dinosaur890123 <dinosaur890123@gmail.com>
2025-09-28 15:34:39 +1000

    Update actions.gd

--- space-station-fighting/actions.gd
+++ space-station-fighting/actions.gd
@@ -11,23 +11,23 @@ func _connect_button(path: String, method_ref):
 		btn.pressed.connect(method_ref)
 
 func _on_divert_shield_button_pressed():
-	var signal_cost = 20.0
+	var signal_cost = 40.0
 	var heal_amount = 20.0
 	if GameData.signal_progress >= signal_cost:
 		GameData.signal_progress -= signal_cost
 		GameData.health += heal_amount
 		GameData.health = min(GameData.health, GameData.MAX_CAPACITY)
-		print("Health boost: -20 signal, +20 health!")
+		print("Health boost: -40 signal, +20 health!")
 	else:
 		print("Not enough signal for health boost!")
 
 func _on_divert_signal_button_pressed():
 	GameData.shield_boost_timer = 0.0
-	var health_cost = 30.0
+	var health_cost = 20.0
 	if GameData.health >= health_cost:
 		GameData.health -= health_cost
-		GameData.signal_progress += 80
-		print("Signal boost: +80 signal for -30 health!")
+		GameData.signal_progress += 40
+		print("Signal boost: +40 signal for -20 health!")
 	else:
 		print("Not enough health for signal boost!")
 
